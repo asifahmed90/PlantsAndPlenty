aefad6659c3bd0e883709f661ee6fc223de96349
asifahmed90 <asifahmed90@users.noreply.github.com>
2021-06-13 15:03:37 -0500

    Header, Footer and Navigation bar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-            <footer> Footer </footer>
+        <footer>
+            <Container> 
+                <Row>
+                    <Col className="text-center py-3">Copyright &copy; PlantsAndPlenty</Col>
+                </Row>
+
+            </Container>
             
-        </div>
+        </footer>
     )
 }
 
